24b75967aa36f76713786007736edcd850e291fd
Adi Gorodesky <adigorodesky@gmail.com>
2024-09-04 21:35:51 +0300

    landscapePopUp

--- src/app/calculator/lossprofit/page.tsx
+++ src/app/calculator/lossprofit/page.tsx
@@ -4,6 +4,7 @@ import { getEmptyRow } from "@/lib/utils"
 import { redirect } from "next/navigation"
 import getServerUser from "@/utils/auth-helpers/getServerUser"
 import { getInitialData } from "@/utils"
+import LandscapePopUp from "@/components/position/LandscapePopUp"
 
 const Page = async ({}) => {
   const {
@@ -18,13 +19,18 @@ const Page = async ({}) => {
   const userStocks = creator ? await getInitialData(creator) : [getEmptyRow("")]
 
   return (
-    <div className="z-20 relative h-full flex-1 flex-col space-y-8 p-8 md:flex">
-      <TableLossProfit
-        columns={columns}
-        creator={creator}
-        userStocks={userStocks}
-      />
-    </div>
+    <>
+      <div className="z-20 relative h-full flex-1 flex-col space-y-8 p-8 md:flex">
+        <TableLossProfit
+          columns={columns}
+          creator={creator}
+          userStocks={userStocks}
+        />
+      </div>
+      <div>
+        <LandscapePopUp />
+      </div>
+    </>
   )
 }
 
---
+++ src/components/position/LandscapePopUp.tsx
@@ -0,0 +1,62 @@
+"use client"
+
+import React, { useState, useEffect } from "react"
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogTitle,
+} from "../ui/dialog"
+import Image from "next/image"
+
+const LandscapePopUp = () => {
+  const [isPhone, setIsPhone] = useState(false)
+  const [dialogOpen, setDialogOpen] = useState(true)
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia("(max-width: 630px)")
+    const handleMediaQueryChange = (e: MediaQueryListEvent) => {
+      setIsPhone(e.matches)
+    }
+    setIsPhone(mediaQuery.matches)
+
+    mediaQuery.addEventListener("change", handleMediaQueryChange)
+
+    return () => {
+      mediaQuery.removeEventListener("change", handleMediaQueryChange)
+    }
+  }, [])
+
+  const handleClose = () => {
+    console.log("test")
+    setDialogOpen(false)
+  }
+
+  if (!isPhone || !dialogOpen) return null
+
+  return (
+    <Dialog open={dialogOpen} onOpenChange={setDialogOpen}>
+      <DialogContent className="flex flex-col items-center py-5 w-[65%]">
+        <button onClick={handleClose} className="absolute top-4 right-4">
+          <Image
+            src="\img\close-rotate-page.svg"
+            height={11}
+            width={11}
+            alt="closing button"
+          />
+        </button>
+        <DialogTitle className="text-center text-black text-md font-normal font-['GFS Didot'] inline w-[70%]">
+          For optimal view, please rotate your mobile screen
+        </DialogTitle>
+        <Image
+          src="\img\Rotate-phone-symbol.svg"
+          height={87}
+          width={86}
+          alt="rotate phone to landscape mode"
+        />
+      </DialogContent>
+    </Dialog>
+  )
+}
+
+export default LandscapePopUp
